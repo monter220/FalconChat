c62cc53ef39c3429a60bdf362fbb96cfe107d317
monter220 <bdstomsk@gmail.com>
2020-04-09 23:39:26 +0700

    use_func

--- x^2.py
+++ x^2.py
@@ -1,16 +1,23 @@
+from math import *
+
+
 a = int(input('a:'))
 b = int(input('b:'))
 c = int(input('c:'))
 
 print('Уравнение имеет вид', a, 'x^2+', b, 'x+', c)
 
-d: int = b**2-4*a*c
-if d < 0:
-    print('нет действительных корней')
-elif d > 0:
-    x1 = (-b+d**1/2)/(2*a)
-    x2 = (-b-d**1/2)/(2*a)
-    print('x1 = ', x1, '\nx2 =', x2)
-else:
-    x = -b/(2*a)
-    print('x =', x)
+def _sqad(q, w, s):
+    d: int = w**2-4*q*s
+    print('Descriminant=', d)
+    if d < 0:
+        print('нет действительных корней')
+    elif d > 0:
+        x1 = (-w+d**1/2)/(2*q)
+        x2 = (-w-d**1/2)/(2*q)
+        print('x1 = ', x1, '\nx2 =', x2)
+    else:
+        x = -w/(2*q)
+        print('x =', x)
+
+_sqad(a,b,c)
