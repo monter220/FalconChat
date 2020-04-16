7612c6182a4fcc76320ab539ece7a660a210d68c
monter220 <bdstomsk@gmail.com>
2020-04-16 11:48:09 +0700

    test for socket

---
+++ socket_server.py
@@ -0,0 +1,20 @@
+import socket
+
+
+sock = socket.socket()
+sock.bind(('', 21090))
+sock.listen(1)
+conn, addr = sock.accept()
+
+
+print('connected:', addr)
+
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper())
+
+
+conn.close()
