d2ef692fe76d40cbbba41b08d2b8eacd9750142d
monter220 <bdstomsk@gmail.com>
2020-04-21 17:16:50 +0700

    add clients list

--- socket_server.py
+++ socket_server.py
@@ -1,6 +1,7 @@
 from socket import *
 from threading import Thread
 import time
+import json
 
 
 def accept_connections():
@@ -8,21 +9,25 @@ def accept_connections():
         client, client_address = SERVER.accept()
         print('%s:%s has connected.' % client_address)
         client.send(bytes('Type your name and press enter!', 'utf8'))
-        addresses[client] = client_address
-        Thread(target=start_client, args=(client,)).start()
+        address = '%s:%s' % (client_address[0], client_address[1])
+        addresses[address] = client
+        Thread(target=start_client, args=(client, address)).start()
 
 
-def start_client(client):
+def start_client(client, address):
     name = client.recv(1024).decode('utf8')
     client.send(bytes('[' + time.ctime() + '] ' + 'Welcome %s!' % name, 'utf8'))
     msg = '%s has joined the chat!' % name
     broadcast(bytes('[' + time.ctime() + '] ' + msg, 'utf8'))
-    clients[client] = name
+    clients[address] = name
+    client.send(bytes(json.dumps(clients), 'utf8'))
 
     while True:
         msg = client.recv(1024)
-        if msg != bytes('[{esc}]', 'utf8'):
+        if msg != bytes('[{esc}]', 'utf8') and msg != bytes('[{reload_clients}]', 'utf8'):
             broadcast(msg, name + ': ')
+        elif msg == bytes('[{reload_clients}]', 'utf8'):
+            client.send(bytes(json.dumps(clients), 'utf8'))
         else:
             client.send(bytes('[{esc}]', 'utf8'))
             client.close()
@@ -32,8 +37,8 @@ def start_client(client):
 
 
 def broadcast(msg, prefix=''):
-    for sock in clients:
-        sock.send(bytes('[' + time.ctime() + '] ' + prefix, 'utf8') + msg)
+    for client in clients:
+        addresses[client].send(bytes('[' + time.ctime() + '] ' + prefix, 'utf8') + msg)
 
 
 clients = {}
@@ -43,7 +48,7 @@ SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(('', 21090))
 
 if __name__ == "__main__":
-    SERVER.listen(100)
+    SERVER.listen()
     print("Waiting for connection...")
     ACCEPT_THREAD = Thread(target=accept_connections)
     ACCEPT_THREAD.start()
